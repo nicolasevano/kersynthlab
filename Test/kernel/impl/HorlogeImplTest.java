b3c5cda611e12a3568fa7c6c37965802ac52507a
syleq1 <syleq1@gmail.com>
2012-02-16 18:30:24 +0100

    [ KerSynthSound ] -Finish all JUnit test for kernel package. -Bug fix on HorlogeImpl with sampleRate getter/setter.

--- Test/kernel/impl/HorlogeImplTest.java
+++ Test/kernel/impl/HorlogeImplTest.java
@@ -2,58 +2,174 @@ package kernel.impl;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
+import kernel.InPort;
+import kernel.Module;
+import kernel.Observer;
+import kernel.OutPort;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class HorlogeImplTest {
+	private HorlogeImpl horlogeImpl;
+	private Module moduleStub1,moduleStub2;
 
+	public class ModuleStub implements Module{
+
+		@Override
+		public void masterNotify() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void addObserver(Observer toAdd) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void removeObserver(Observer toRemove) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void moduleFunction() {
+			// TODO Auto-generated method stub
+			moduleFuntionCall = true;
+		}
+
+		@Override
+		public Map<String, InPort> getInPorts() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Map<String, OutPort> getOutPorts() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		public boolean moduleFuntionCall = false;
+	}
 	@Before
 	public void setUp() throws Exception {
+		
+		horlogeImpl = new HorlogeImpl();
+		moduleStub1 = new ModuleStub();
+		moduleStub2 = new ModuleStub();
+		horlogeImpl.addModuleObserver( moduleStub1 );
+		horlogeImpl.addModuleObserver( moduleStub2 );
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		horlogeImpl.removeModuleObserver( moduleStub1 );
+		horlogeImpl.removeModuleObserver( moduleStub2 );
+		horlogeImpl = null;
+		moduleStub1 = null;
+		moduleStub2 = null;
 	}
 
 	@Test
 	public void testHorlogeImpl() {
-		fail("Not yet implemented");
+		assertNotNull( horlogeImpl );
 	}
 
 	@Test
 	public void testAddModuleObserver() {
-		fail("Not yet implemented");
+		horlogeImpl.tick();
+		assertTrue( ( ( ModuleStub ) moduleStub1 ).moduleFuntionCall );
+		assertTrue( ( ( ModuleStub ) moduleStub2 ).moduleFuntionCall );
 	}
 
 	@Test
 	public void testRemoveModuleObserver() {
-		fail("Not yet implemented");
+		horlogeImpl.removeModuleObserver( moduleStub1 );
+		horlogeImpl.tick();
+		assertFalse( ( ( ModuleStub ) moduleStub1 ).moduleFuntionCall );
+		assertTrue( ( ( ModuleStub ) moduleStub2 ).moduleFuntionCall );
 	}
 
 	@Test
 	public void testTick() {
-		fail("Not yet implemented");
+		assertFalse( ( ( ModuleStub ) moduleStub1 ).moduleFuntionCall );
+		assertFalse( ( ( ModuleStub ) moduleStub2 ).moduleFuntionCall );
+		horlogeImpl.tick();
+		assertTrue( ( ( ModuleStub ) moduleStub1 ).moduleFuntionCall );
+		assertTrue( ( ( ModuleStub ) moduleStub2 ).moduleFuntionCall );
 	}
 
 	@Test
 	public void testStart() {
-		fail("Not yet implemented");
+		
+		horlogeImpl.start();
+		try {
+			Thread.sleep( 40 );
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue( ( ( ModuleStub ) moduleStub1 ).moduleFuntionCall );
+		assertTrue( ( ( ModuleStub ) moduleStub2 ).moduleFuntionCall );
+		horlogeImpl.stop();
+		try {
+			Thread.sleep( 40 );
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Test
 	public void testStop() {
-		fail("Not yet implemented");
+		
+		horlogeImpl.start();
+		try {
+			Thread.sleep( 40 );
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue( ( ( ModuleStub ) moduleStub1 ).moduleFuntionCall );
+		assertTrue( ( ( ModuleStub ) moduleStub2 ).moduleFuntionCall );
+		horlogeImpl.stop();
+		try {
+			Thread.sleep( 40 );
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		( ( ModuleStub ) moduleStub1 ).moduleFuntionCall = false;
+		( ( ModuleStub ) moduleStub2 ).moduleFuntionCall = false;
+		
+		try {
+			Thread.sleep( 40 );
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertFalse( ( ( ModuleStub ) moduleStub1 ).moduleFuntionCall );
+		assertFalse( ( ( ModuleStub ) moduleStub2 ).moduleFuntionCall );
 	}
 
 	@Test
 	public void testGetSampleRate() {
-		fail("Not yet implemented");
+		horlogeImpl.setSampleRate( 44100 );
+		assertEquals(44100, horlogeImpl.getSampleRate() );
 	}
 
 	@Test
 	public void testSetSampleRate() {
-		fail("Not yet implemented");
+		horlogeImpl.setSampleRate( 44100 );
+		assertEquals(44100, horlogeImpl.getSampleRate() );
 	}
-
 }
--- Test/kernel/impl/SquareWaveFormTest.java
+++ Test/kernel/impl/SquareWaveFormTest.java
@@ -1,29 +1,45 @@
 package kernel.impl;
 
 import static org.junit.Assert.*;
+import kernel.InPort;
+import kernel.impl.VCO.WaveForm;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SquareWaveFormTest {
+	private VCO vco;
+	private InPort inPortImpl;
 
 	@Before
 	public void setUp() throws Exception {
+		HorlogeImpl.setSampleRate( 44100 );
+		vco = new VCO();
+		inPortImpl = new InPortImpl();
+		vco.addObserver( inPortImpl );
 	}
 
 	@After
 	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testSquareWaveForm() {
-		fail("Not yet implemented");
+		vco.removeObserver( inPortImpl );
+		vco = null;
+		inPortImpl = null;
 	}
 
 	@Test
 	public void testWaveForm() {
-		fail("Not yet implemented");
+		vco.setWaveForm(WaveForm.SQUARE);
+		vco.setBase( 8 );
+		vco.setPitch( 1 );
+		vco.setAtt( 1 );
+		vco.moduleFunction();
+		//assertFalse( inPortStub.isEmpty() );
+		//TODO remove line below as soon as 
+		//Inport interface will be updated with setModule
+		assertEquals( -32768,inPortImpl.getValue() );
+		vco.moduleFunction();
+		assertEquals( (-32768) , inPortImpl.getValue() );
 	}
 
 }
